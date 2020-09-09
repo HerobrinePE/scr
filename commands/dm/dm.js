61a84eabaf48cc95ab548754e563aea032740191
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-09-09 17:36:20 -0400

    Update dm.js

--- commands/dm/dm.js
+++ commands/dm/dm.js
@@ -1,17 +1,31 @@
 module.exports = {
-  name: "dm",
-  aliases: ["a"],
+  name: "msg",
+  aliases: ["send"],
   category: "dm",
   description: "dm",
   run: (client, message, args) => {
-    
-        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You cant access this")
-    
-    
-  let usr = message.mentions.users.first()
-  if(!usr) return message.reply("please mention user")
-  let text = message.content.split(" ").slice(2)
-  if(!text) return message.reply("Cant do that")
-    usr.send(text.join(" "))
+    let owner = message.author;
+    let __myId__="753368008986198044"
+    let ids = "743615073045708870";
+    let role = message.member.roles.has(ids);
+    if (!message.member.roles.some(role => role.id === ids)) return error();
+    function error() {
+      const { RichEmbed } = require("discord.js");
+      const b = new RichEmbed()
+        .setTitle("Owners only command")
+        .setDescription(
+          "only <" + ids.name + "> Role has access to this commandor the owner"
+        )
+        .setFooter("Thank you");
+      message.reply(b);
+    }
+    let usr = args[0];
+    if (usr.length == 18) {
+      if (!usr) return message.reply("please give id");
+      let text = message.content.split(" ").slice(2);
+      if (!text) return message.reply("Cant do that");
+      client.users.get(usr).send(text.join(" "));
+      message.reply("Sent");
+    } else return message.reply("not a valid id");
   }
-}
+};
